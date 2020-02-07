7789277a20bc73cedbb57a25b8d2ff25c5b5c9cd
xcob <34405172+xcob@users.noreply.github.com>
2020-02-06 20:46:37 -0600

    updated homepage styling

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import logo from './logo.svg';
 import {useSpring, animated} from 'react-spring'
 import './App.css';
 
 
 const calc = (x, y) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
 const trans1 = (x, y) => `translate3d(${x / 10}px,${y / 10}px,0)`
-const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`
-const trans3 = (x, y) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
-const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
+
+
+// const trans2 = (x, y) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`
+ const trans3 = (x, y) => `translate3d(${x /  17}px,${y /  17}px,0)`
+// const trans4 = (x, y) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
 
 function App() {
   const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }))
   return (
     <div class="container" onMouseMove={({ clientX: x, clientY: y }) => set({ xy: calc(x, y) })}>
       <animated.div class="card1" style={{ transform: props.xy.interpolate(trans1) }}>
-        J ACOB
+      <h2 style={{ color: '#fff', fontSize: '5rem', zIndex: 4000}}>J<span>acob</span></h2>
       </animated.div>
     
-      <animated.div class="card2" style={{ transform: props.xy.interpolate(trans2) }}>
-        S mith
+      <animated.div class="card2" style={{ transform: props.xy.interpolate(trans1) }}>
+      <h2 style={{ color: '#fff', fontSize: '5rem', zIndex: 4000}}>S<span>mith</span></h2>
       </animated.div>
-      
-     
-      <animated.div class="card3" style={{ transform: props.xy.interpolate(trans3) }}>
-
-      </animated.div>
-      <animated.div class="card4" style={{ transform: props.xy.interpolate(trans4) }}>
-
-      </animated.div>
+      <animated.div class="card5" style={{ transform: props.xy.interpolate(trans3) }}></animated.div>
+      <animated.div class="card3" style={{ transform: props.xy.interpolate(trans1) }}></animated.div>
+      <animated.div class="card4" style={{ transform: props.xy.interpolate(trans1) }}></animated.div>
+      <animated.div class="card6" style={{ transform: props.xy.interpolate(trans3) }}></animated.div>
+      <h1>About | Work | Projects</h1>
     </div>
   )
 }
